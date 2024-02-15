6fa1bb24c46d59a9dbd0d9154e1b5bae47402e0a
Pablo Ortiz López <pabloortiz132@gmail.com>
2024-02-15 19:33:42 +0100

    [web-dsky] Finish layout & blink COMP_ACTY segment for demonstration purposes

--- web-dsky/src/app/page.tsx
+++ web-dsky/src/app/page.tsx
@@ -1,16 +1,20 @@
+"use client"
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [isCompActy,setCompActy] = useState(false)
+  setTimeout(()=> setCompActy(!isCompActy), 1000)
+
+  useEffect(()=>{
+    setTimeout(()=> setCompActy(!isCompActy), 1000)
+  },[isCompActy])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Image
-          alt={'verb'}
-          src={'/goal.jpg'}
-          width={1000}
-          height={1000}
-          className="goal"
-        ></Image>
         <Image
           alt={'mask'}
           src={'/mask.svg'}
@@ -18,6 +22,7 @@ export default function Home() {
           height={1000}
           className="mask"
         ></Image>
+        {isCompActy && <div className={'comp_acty'} />}
       </div>
     </main>
   );
