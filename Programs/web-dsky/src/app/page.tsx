4ed82266c0f0d44806a325e00ce84ef0acc5824e
Pablo Ortiz López <pabloortiz132@gmail.com>
2024-03-10 12:45:07 +0100

    [web] Improve responsiveness

--- Programs/web-dsky/src/app/page.tsx
+++ Programs/web-dsky/src/app/page.tsx
@@ -68,8 +68,8 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="ELDisplay">
         <Image
           alt={'mask'}
           src={'/mask.svg'}
