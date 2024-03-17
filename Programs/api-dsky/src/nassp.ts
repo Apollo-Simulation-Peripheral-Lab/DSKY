f6aa4aadc142969844b592d246b1f55f163922f3
Pablo Ortiz López <pabloortiz132@gmail.com>
2024-03-17 23:18:44 +0100

    #24 Update nassp chords to fix the pro key

--- Programs/api-dsky/src/nassp.ts
+++ Programs/api-dsky/src/nassp.ts
@@ -12,15 +12,15 @@ const keyMap = {
     '8': [Key.RightShift, Key.NumPad8],
     '9': [Key.RightShift, Key.NumPad9],
     '0': [Key.RightShift, Key.NumPad0],
-    'e': [Key.RightShift, Key.End],
-    'p': [Key.RightShift, Key.P],
+    'e': [Key.RightShift, Key.T],
+    'p': [Key.RightShift, Key.End],
     'v': [Key.RightShift, Key.V],
     'n': [Key.RightShift, Key.N],
     '+': [Key.RightShift, Key.Add],
     '-': [Key.RightShift, Key.Subtract],
-    'c': [Key.RightShift, Key.C],
+    'c': [Key.RightShift, Key.Decimal],
     'r': [Key.RightShift, Key.R],
-    'k': [Key.RightShift, Key.Decimal]
+    'k': [Key.RightShift, Key.Home]
 };
 
 export const watchStateNASSP = (_callback) => {
