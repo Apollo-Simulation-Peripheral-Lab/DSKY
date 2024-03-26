e0c8e918f976c0aa9c1c82b594116299c4a6a1a1
Pablo Ortiz López <pabloortiz132@gmail.com>
2024-03-26 21:04:57 +0100

    Add fpga test firmware

---
+++ firmware/TangNano9KTestLCD/src/VGAMod.v
@@ -0,0 +1,93 @@
+module VGAMod
+(
+    input                   CLK,
+    input                   nRST,
+
+    input                   PixelClk,
+
+    output                  LCD_DE,
+    output                  LCD_HSYNC,
+    output                  LCD_VSYNC,
+
+	output          [4:0]   LCD_B,
+	output          [5:0]   LCD_G,
+	output          [4:0]   LCD_R
+);
+
+    reg         [15:0]  PixelCount;
+    reg         [15:0]  LineCount;
+
+	localparam      V_BackPorch = 16'd0; //6
+	localparam      V_Pluse 	= 16'd5; 
+	localparam      HightPixel  = 16'd480;
+	localparam      V_FrontPorch= 16'd45; //62
+
+	localparam      H_BackPorch = 16'd182; 	//NOTE: Increase the delay here when using high-resolution clock to facilitate K210 interrupt
+	localparam      H_Pluse 	= 16'd1; 
+	localparam      WidthPixel  = 16'd800; 
+	localparam      H_FrontPorch= 16'd210;
+
+    parameter       BarCount    = 16; // RGB565
+    localparam      Width_bar   = WidthPixel / 16;
+     
+    localparam      PixelForHS  =   WidthPixel + H_BackPorch + H_FrontPorch;  	
+    localparam      LineForVS   =   HightPixel + V_BackPorch + V_FrontPorch;
+
+    always @(  posedge PixelClk or negedge nRST  )begin
+        if( !nRST ) begin
+            LineCount       <=  16'b0;    
+            PixelCount      <=  16'b0;
+            end
+        else if(  PixelCount  ==  PixelForHS ) begin
+            PixelCount      <=  16'b0;
+            LineCount       <=  LineCount + 1'b1;
+            end
+        else if(  LineCount  == LineForVS  ) begin
+            LineCount       <=  16'b0;
+            PixelCount      <=  16'b0;
+            end
+        else
+            PixelCount      <=  PixelCount + 1'b1;
+    end
+
+	reg			[9:0]  Data_R;
+	reg			[9:0]  Data_G;
+	reg			[9:0]  Data_B;
+
+    always @(  posedge PixelClk or negedge nRST  )begin
+        if( !nRST ) begin
+			Data_R <= 9'b0;
+			Data_G <= 9'b0;
+			Data_B <= 9'b0;
+            end
+	end
+	// Pay attention here: HSYNC and VSYNC are negative polarity
+    assign  LCD_HSYNC = (( PixelCount >= H_Pluse)&&( PixelCount <= (PixelForHS-H_FrontPorch))) ? 1'b0 : 1'b1;
+    //assign  LCD_VSYNC = ((( LineCount  >= 0 )&&( LineCount  <= (V_Pluse-1) )) ) ? 1'b1 : 1'b0;		//If not reduced by one here, the bottom of the picture will drag down?
+	assign  LCD_VSYNC = ((( LineCount  >= V_Pluse )&&( LineCount  <= (LineForVS-0) )) ) ? 1'b0 : 1'b1;
+    //assign  FIFO_RST  = (( PixelCount ==0)) ? 1'b1 : 1'b0;  //Leave time for the host to enter interrupt during H_BackPorch, send data
+
+    assign  LCD_DE = (  ( PixelCount >= H_BackPorch )&&
+                        ( PixelCount <= PixelForHS-H_FrontPorch ) &&
+                        ( LineCount >= V_BackPorch ) &&
+                        ( LineCount <= LineForVS-V_FrontPorch-1 ))  ? 1'b1 : 1'b0;
+						//If not reduced by one here, it will jitter
+
+    // Define square
+    parameter squareTop = 100;
+    parameter squareBottom = 400;
+    parameter squareLeft = 100;
+    parameter squareRight = 400;
+
+    // Display green square
+    assign LCD_R = 5'b00000;
+    
+    assign LCD_G = ( PixelCount >= squareLeft &&
+        PixelCount <= squareRight &&
+        LineCount >= squareTop &&
+        LineCount <= squareBottom
+    ) ? 6'b111111 : 6'b000000; // Green
+
+    assign LCD_B = 5'b00000;
+    
+endmodule
