116edab74e8192d1788d58af6ee73698aacda4c0
Pablo Ortiz López <pabloortiz132@gmail.com>
2024-04-06 22:44:23 +0200

    #43 Remove experimental warning

--- Programs/api-dsky/src/terminalSetup.ts
+++ Programs/api-dsky/src/terminalSetup.ts
@@ -12,7 +12,7 @@ export const getInputSource = async() =>{
                 {name:'Reentry', value: 'reentry'},
                 {name:'KSP', value: 'ksp'},
                 {name:'NASSP (experimental)', value: 'nassp'},
-                {name:'yaAGC (experimental)', value: 'yaagc'},
+                {name:'yaAGC', value: 'yaagc'},
                 {name:'Random Values', value: 'random'},
                 {name:'Bridge to another DSKY API', value: 'bridge'}
             ]
