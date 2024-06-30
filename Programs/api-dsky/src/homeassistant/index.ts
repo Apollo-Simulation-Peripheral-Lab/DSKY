eb9e8c88dde9b0ba50ad3f2310c84e19e73739c7
Pablo Ortiz <pabloortiz132@gmail.com>
2024-06-30 19:19:11 +0200

    refactor custom AGC

--- Programs/api-dsky/src/homeassistant/index.ts
+++ Programs/api-dsky/src/homeassistant/index.ts
@@ -13,21 +13,40 @@ export const verbs = {
     '40': v40
 }
 
+export const nouns = {
+    '01': [0,0,0],
+    '02': [0,0,0]
+}
+
 export const internalState = {
     inputMode : '',
     verbNounFlashing : false,
     flashState : false,
     operatorErrorActive : false,
-    verbValue : '',
-    nounValue : '',
-    programValue : ''
+    verb : '',
+    noun : '',
+    program : '',
+    extendedVerb: '',
+    register1: '',
+    register2: '',
+    register3: ''
 }
 
 let setState = (_state) => {}
 
 let flashTicks = 0
 const drawState = () => {
-    const {flashState, operatorErrorActive, verbNounFlashing, programValue, verbValue,nounValue} = internalState
+    const {
+        flashState, 
+        operatorErrorActive, 
+        verbNounFlashing, 
+        program, 
+        verb,
+        noun,
+        register1,
+        register2,
+        register3
+    } = internalState
     flashTicks++
     if(flashTicks >= 20){
         internalState.flashState = !flashState;
@@ -38,12 +57,30 @@ const drawState = () => {
     state = {
         ...state,
         IlluminateOprErr: operatorErrorActive && flashState ? 1 : 0,
-        VerbD1: (!verbNounFlashing || flashState) ? (verbValue[0] || '') : '',
-        VerbD2: (!verbNounFlashing || flashState) ? (verbValue[1] || '') : '',
-        NounD1: (!verbNounFlashing || flashState) ? (nounValue[0] || '') : '',
-        NounD2: (!verbNounFlashing || flashState) ? (nounValue[1] || '') : '',
-        ProgramD1: programValue[0] || '',
-        ProgramD2: programValue[1] || '',
+        VerbD1: (!verbNounFlashing || flashState) ? (verb[0] || '') : '',
+        VerbD2: (!verbNounFlashing || flashState) ? (verb[1] || '') : '',
+        NounD1: (!verbNounFlashing || flashState) ? (noun[0] || '') : '',
+        NounD2: (!verbNounFlashing || flashState) ? (noun[1] || '') : '',
+        ProgramD1: program[0] || '',
+        ProgramD2: program[1] || '',
+        Register1Sign: register1[0] || '',
+        Register1D1: register1[1] || '',
+        Register1D2: register1[2] || '',
+        Register1D3: register1[3] || '',
+        Register1D4: register1[4] || '',
+        Register1D5: register1[5] || '',
+        Register2Sign: register2[0] || '',
+        Register2D1: register2[1] || '',
+        Register2D2: register2[2] || '',
+        Register2D3: register2[3] || '',
+        Register2D4: register2[4] || '',
+        Register2D5: register2[5] || '',
+        Register3Sign: register3[0] || '',
+        Register3D1: register3[1] || '',
+        Register3D2: register3[2] || '',
+        Register3D3: register3[3] || '',
+        Register3D4: register3[4] || '',
+        Register3D5: register3[5] || ''
     }
     setState(state)
 };
@@ -58,25 +95,40 @@ export const watchStateHA = async (callback) =>{
 }
 
 const keyboardHandler = (input: string) => {
-    const { inputMode, verbValue, nounValue } = internalState
-    if (input === 'v') {
+    const { inputMode, verb, noun} = internalState
+    if (input === 'r') {
+        internalState.operatorErrorActive = false;
+        internalState.verbNounFlashing = false;
+    }else if (input === 'c'){
+        console.log({inputMode})
+        if(inputMode){
+            internalState[inputMode] = ''
+        }
+    }else if (input === 'v') {
         internalState.inputMode = 'verb';
-        internalState.verbValue = '';
+        internalState.verb = '';
+        internalState.verbNounFlashing = false;
+    }else if (input === 'n') {
+        internalState.inputMode = 'noun';
+        internalState.noun = '';
+        internalState.verbNounFlashing = false;
     } else if (inputMode === 'verb' && /^[0-9]$/.test(input)) {
-        if(!verbValue[1])
-        internalState.verbValue += input;
+        if(!verb[1]) internalState.verb += input;
     } else if (input === 'e') {
-        if(verbs[verbValue]){
-            verbs[verbValue]()
+        if(verbs[verb]){
+            verbs[verb]()
         } else {
             internalState.operatorErrorActive = true;
         }
     } else if (inputMode === 'noun' && /^[0-9]$/.test(input)) {
-        if(!nounValue[1])
-            internalState.nounValue += input;
-    } else if (input === 'r') {
-        internalState.operatorErrorActive = false;
-        internalState.verbNounFlashing = false;
+        if(!noun[1]) internalState.noun += input;
+    }else if(['register1','register2', 'register3'].includes(inputMode)){
+        if(
+            (internalState[inputMode] === '' && /^[+-]$/.test(input)) ||
+            (internalState[inputMode].length > 0 && internalState[inputMode].length < 6 && /^[0-9]$/.test(input))
+        ){
+            internalState[inputMode] += input
+        }
     }
 };
 
