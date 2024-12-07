1e73086538ecec17f2809b20798a114e2564866f
Pablo Ortiz López <pablo.ortiz@vibia.com>
2024-12-07 15:27:45 +0100

    Fix tabs

--- orangepi.sh
+++ orangepi.sh
@@ -5,10 +5,10 @@ if [ "$1" = "api" ];then
     xttitle api-dsky
     unclutter -idle 3 -root &>/dev/null &
     while true; do
-    	wmctrl -a api-dsky
-	    killall chromium-browser &>/dev/null
+        wmctrl -a api-dsky
+        killall chromium-browser &>/dev/null
         cd ~/DSKY/Programs/api-dsky
-	    npm start -- \
+        npm start -- \
             -s /dev/ttyUSB0 \
             --callback 'chromium-browser --start-fullscreen --incognito http://localhost:3000 && sleep 5 && wmctrl -a chromium' \
             --shutdown 'shutdown -h now' $@
